57e4c604ca4fe539fed3acb9f96c6993726a11b2
NicoleXaviera <nicole.arguedas@tecsup.edu.pe>
2023-06-29 09:45:34 -0500

    segundocommit

--- react/src/components/Navigation.jsx
+++ react/src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function Navigation() {
   const [isAuth, setIsAuth] = useState(false);
@@ -13,24 +13,26 @@ export function Navigation() {
 
   return (
     <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="/">Sistema de Matrícula</Navbar.Brand>
-          <Nav className="me-auto">
-          {isAuth ?
+      <Navbar.Brand href="/">Sistema de Matrícula</Navbar.Brand>
+      <Nav className="me-auto">
+        {isAuth && (
+          <>
             <Nav.Link href="/">Home</Nav.Link>
-            : null}
-          </Nav>
-          <Nav>
-          {isAuth ?
-            <Nav.Link href="/logout">Logout</Nav.Link>:
+            <Nav.Link href="/departamento">Departamento</Nav.Link>
+            <Nav.Link href="/semestres">Semestre</Nav.Link>
+          </>
+        )}
+      </Nav>
+      <Nav>
+        {isAuth ? (
+          <Nav.Link href="/logout">Logout</Nav.Link>
+        ) : (
+          <>
             <Nav.Link href="/login">Login</Nav.Link>
-          }
-          </Nav>
-          <Nav>
-            {isAuth ?
-              <div></div>:
-              <Nav.Link href="/register">Register</Nav.Link>
-            }
-          </Nav>
-      </Navbar>
+            <Nav.Link href="/register">Register</Nav.Link>
+          </>
+        )}
+      </Nav>
+    </Navbar>
   );
 }
