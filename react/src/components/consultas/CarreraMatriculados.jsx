4f13b9bdee9486726e907935cb6e37b3fc182f86
NicoleXaviera <nicole.arguedas@tecsup.edu.pe>
2023-07-05 22:43:59 -0500

    update8_cupos_ciclo_y_carreras

--- react/src/components/consultas/CarreraMatriculados.jsx
+++ react/src/components/consultas/CarreraMatriculados.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useReactToPrint } from "react-to-print";
-
 import {
   BarChart,
   Bar,
@@ -16,7 +14,6 @@ import {
 const MatriculadosPorCarrera = () => {
   const [matriculados, setMatriculados] = useState([]);
   const [error, setError] = useState("");
-  const componentPDF = useRef();
 
   useEffect(() => {
     obtenerMatriculadosPorCarrera();
@@ -41,19 +38,38 @@ const MatriculadosPorCarrera = () => {
     }
   };
 
-  const generatePDF = useReactToPrint({
-    content: () =>componentPDF.current,
-    documentTitle:"Matriculador por Carrera",
-    //onAfterPrint:()=>alert("Datos guardados en PDF")
-  });
+  const wrapTick = (text, width) => {
+    const words = text.split(" ");
+    const lineHeight = 16;
+    const maxLines = Math.floor(width / lineHeight);
+    let wrappedText = "";
 
-  const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff6e63", "#a2a2a2"];
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      const nextText = `${wrappedText} ${word}`;
+      const nextWidth = measureTextWidth(nextText);
+
+      if (nextWidth > width && i > 0) {
+        wrappedText += `\n${word}`;
+      } else {
+        wrappedText += ` ${word}`;
+      }
+    }
+
+    return wrappedText.trim();
+  };
+
+  const measureTextWidth = (text) => {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    context.font = "12px sans-serif";
+    return context.measureText(text).width;
+  };
 
   return (
     <div className="container">
-      <div ref={ componentPDF } style={{width:'100%'}}>
       <div className="row mt-3">
-        <div className="col-lg-6">
+        <div className="col-lg-12">
           <div className="table-responsive">
             <h2 className="mt-4 mb-3">Matriculados por Carrera</h2>
             {error && <p className="text-danger">{error}</p>}
@@ -78,34 +94,42 @@ const MatriculadosPorCarrera = () => {
               <p>No hay matriculados por carrera disponibles.</p>
             )}
           </div>
-          <div>
-            <button className='btn btn-success' onClick={ generatePDF }>PDF</button>
-          </div>
         </div>
       </div>
       <div className="row mt-3">
-        <div className="col-lg-6">
-          <div style={{ width: '100%', height: 400 }} className="mx-auto mt-4">
+        <div className="col-lg-12">
+          <div style={{ width: "100%", height: 400 }} className="mx-auto mt-4">
             <ResponsiveContainer>
-              <BarChart
+            <BarChart
                 data={matriculados}
-                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                margin={{ top: 20, right: 30, left: 10, bottom: 2 }}
+                fontSize={13}
               >
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="nombre" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar
-                  dataKey="matriculados"
-                  fill="#8884d8"
-                  barSize={30}
+                <XAxis
+                  dataKey="nombre"
+                  interval={0}
+                  angle={-45}
+                  tick={{
+                    width: 100,
+                    wrapStyle: {
+                    whiteSpace: "pre-wrap",
+                    },
+                  }}
                 />
+                <YAxis 
+                  label={{
+                    value: "Cantidad de Matriculados por carrera",
+                    angle: -90,
+                    position: "insideLeft",
+                    dy: 90, }}/>
+                <Tooltip formatter={(value) => [value, "Cantidad de Matriculados"]} />
+                <Legend />
+                <Bar dataKey="matriculados" fill="#3b83bd" barSize={40} />
               </BarChart>
-            </ResponsiveContainer>
+              </ResponsiveContainer>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
